0bbe3f3e28c6ab586d605da96e1b3c3b21a2f8ac
Adoah <xadoahx@gmail.com>
2018-07-11 22:05:47 +0300

    added color, 5 branches

--- branch.js
+++ branch.js
@@ -1,21 +1,27 @@
-function branch(begin, end, len) {
+function branch(begin, end, len, maxheight) {
     this.begin = begin;
     this.end = end;
     this.len = len;
     this.finished = false;
     this.thickness = 0;
+    this.angA = PI / 3;
+    this.angB = PI / 6;
+    this.color = stroke(255);
+    var isHidden = true;
 
     this.show = function () {
-        if (len > 199) { this.thickness = 8; }
-        else if (len > 132) { this.thickness = 7; }
-        else if (len > 87) { this.thickness = 6; }
-        else if (len > 58) { this.thickness = 5; }
-        else if (len > 38) { this.thickness = 4; }
-        else if (len > 25) { this.thickness = 3; }
-        else if (len > 16) { this.thickness = 2; }
-        else if (len > 10) { this.thickness = 1; }
+        if (len > heightSteps[0]) { this.thickness = 8; this.color = stroke(193, 76, 254) }
+        //red
+        else if (len > heightSteps[1]) { this.thickness = 7; this.color = stroke(71, 76, 254); }
+        //blue
+        else if (len > heightSteps[2]) { this.thickness = 6; this.color = stroke(71, 207, 229); }
+        //green
+        else if (len > heightSteps[3]) { this.thickness = 5; this.color = stroke(28, 207, 80); }
+        else if (len > heightSteps[4]) { this.thickness = 4; this.color = stroke(155, 255, 123); }
+        else if (len > heightSteps[5]) { this.thickness = 3; this.color = stroke(203, 203, 0); }
+        else if (len > heightSteps[6]) { this.thickness = 2; this.color = stroke(205, 146, 0); }
+        else if (len > heightSteps[7]) { this.thickness = 1; this.color = stroke(255, 0, 0); }
         strokeWeight(this.thickness);
-        stroke(255);
         line(this.begin.x, this.begin.y, this.end.x, this.end.y);
 
     }
@@ -24,22 +30,24 @@ function branch(begin, end, len) {
     this.generateBranches = function () {
         var dir = p5.Vector.sub(this.end, this.begin);
         dir.mult(0.67);
-        dir.rotate(PI / 5);
+        var newEnd = p5.Vector.add(this.end, dir);
+        var genStraight = new branch(this.end, newEnd, dir.mag(), this.maxheight);
+        dir.rotate(this.angA);
         var newEndA = p5.Vector.add(this.end, dir);
-        var genA = new branch(this.end, newEndA, dir.mag());
-        dir.rotate(-PI / 5);
-        dir.rotate(-PI / 5);
+        var genA = new branch(this.end, newEndA, dir.mag(), this.maxheight);
+        dir.rotate(-this.angA);
+        dir.rotate(-this.angA);
         var newEndB = p5.Vector.add(this.end, dir);
-        var genB = new branch(this.end, newEndB, dir.mag());
-        dir.rotate(PI / 5);
-        dir.rotate(PI / 8);
+        var genB = new branch(this.end, newEndB, dir.mag(), this.maxheight);
+        dir.rotate(this.angA);
+        dir.rotate(this.angB);
         var newEndC = p5.Vector.add(this.end, dir);
-        var genC = new branch(this.end, newEndC, dir.mag());
-        dir.rotate(-PI / 8);
-        dir.rotate(-PI / 8);
+        var genC = new branch(this.end, newEndC, dir.mag(), this.maxheight);
+        dir.rotate(-this.angB);
+        dir.rotate(-this.angB);
         var newEndD = p5.Vector.add(this.end, dir);
-        var genD = new branch(this.end, newEndD, dir.mag());
-        return [genA, genB, genC, genD];
+        var genD = new branch(this.end, newEndD, dir.mag(), this.maxheight);
+        return [genStraight, genA, genB, genC, genD];
     }
 
 
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var tree = [];
 var counter = 0;
+var maxHeight;
 
 function setup() {
-    createCanvas(1200, 900);
+    createCanvas($(document).width(), $(window).height());
+    console.log(heightSteps);
 }
 
 function draw() {
@@ -14,22 +16,24 @@ function draw() {
 }
 
 function mouseWheel(event) {
-    //scrollind down
+    //scrolling down
     if (event.delta > 0) {
         if (counter == 0) {
+            maxHeight = (height / 3);
             var a = createVector(width / 2, height);
-            var b = createVector(width / 2, height - 200);
-            tree[0] = new branch(a, b, 200);
+            var b = createVector(width / 2, height - height / 3);
+            tree[0] = new branch(a, b, height / 3);
+            console.log(height);
         }
         else {
             for (var i = tree.length - 1; i >= 0; i--) {
-                if (counter <= 9) {
+                if (counter < 9) {
                     var tmp = tree[i].generateBranches();
-
                     tree.push(tmp[0]);
                     tree.push(tmp[1]);
                     tree.push(tmp[2]);
                     tree.push(tmp[3]);
+                    tree.push(tmp[4]);
                 }
                 tree[i].finished = true;
             }
@@ -38,6 +42,7 @@ function mouseWheel(event) {
     }
     if (event.delta < 0) {
         console.log(counter);
+        console.log(heightSteps);
         for (var i = tree.length - 1; i >= 0; i--) {
             if (8 - tree[i].getThickness() == counter) {
                 tree.splice(i, 1);
@@ -48,3 +53,19 @@ function mouseWheel(event) {
         }
     }
 }
+
+
+
+
+
+/*
+(len > this.maxheight * Math.pow(0.67, 1)) { this.thickness = 8; this.color = stroke('#8000ff') }
+        else if (len > this.maxheight * Math.pow(0.67, 2)) { this.thickness = 7; this.color = stroke('#0080ff'); }
+        else if (len > this.maxheight * Math.pow(0.67, 3)) { this.thickness = 6; this.color = stroke('#00ffff'); }
+        else if (len > this.maxheight * Math.pow(0.67, 4)) { this.thickness = 5; this.color = stroke('#8000ff'); }
+        else if (len > this.maxheight * Math.pow(0.67, 5)) { this.thickness = 4; this.color = stroke(0, 255, 255, 130); }
+        else if (len > this.maxheight * Math.pow(0.67, 6)) { this.thickness = 3; this.color = stroke(0, 200, 200, 100); }
+        else if (len > this.maxheight * Math.pow(0.67, 7)) { this.thickness = 2; this.color = stroke(0, 150, 150, 70); }
+        else if (len > this.maxheight * Math.pow(0.67, 8)
+
+        */
